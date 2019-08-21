112c6c978151e00eba75172139bb97e1c0809d8f
night <40443125@gm.nfu.edu.tw>
2019-08-22 06:40:51 +0800

    homework2

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -11,12 +11,12 @@ public class Tester {
         while(true){
             System.out.println("Please enter number of tickets: ");
             int num = scanner.nextInt();
+            if (num == -1) break;
             System.out.println("How many round_trip tickets: ");
             int numOfRound = scanner.nextInt();
             System.out.println("Total tickets: " + num);
             System.out.println("Round-trip: " + numOfRound);
             System.out.println("Total: " + tk.totalOfPrice(num, numOfRound));
-            System.out.println("Continue?(y/n)");
         }
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -5,6 +5,7 @@ fun main(){
     while(true){
         println("Please enter number of tickets: ")
         val num = readLine()?.toInt()
+        if(num==-1) break
         println("How many round-trip tickets: ")
         val numOfRound = readLine()?.toInt()
         print("Total tickets: $num \n Round-trip: $numOfRound \n Total: ${tk.totalOfPrice(num!!, numOfRound!!)} \n")
